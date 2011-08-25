5571f8d3d95d1ec2bb52b0c8867f7ba1bcd75fac
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2011-08-25 15:34:44 +0300

    add support for filters

--- index.php
+++ index.php
@@ -31,11 +31,11 @@ function load_image($fn) {
   return array($w, $h, $type, $img);
 }
 
-function save_image_file($file_type, $src, $dst, $w, $h) {
+function save_image_file($file_type, $src, $dst, $preset, $w, $h) {
   if ($file_type!='png' && $file_type!='jpeg') return error('unsupported file type');
 	if ($w>Conf::$max_width || $h>Conf::$max_height) return error('too big');
-  if (isset(Conf::$sizes) && (!isset(Conf::$sizes[$w.'x'.$h]) || !Conf::$sizes[$w.'x'.$h] )) error('not allowed size');
-  $wms=Conf::$sizes[$w.'x'.$h];
+  if (isset(Conf::$sizes) && (!isset(Conf::$sizes[$preset]) || !Conf::$sizes[$preset] )) error("not allowed size");
+  $wms=Conf::$sizes[$preset];
   @mkdir(dirname($dst), 0777, true);
   if (!is_dir(dirname($dst))) error('could not create directory');
   list($w_src, $h_src, $type, $img_src)=load_image($src);
@@ -64,10 +64,16 @@ function save_image_file($file_type, $src, $dst, $w, $h) {
   if (is_array($wms)) {
     foreach($wms as $wm) {
     if (is_array($wm)) {
+    if ($wm[0]=="filter") {
+      $ua=$wm; //array_merge(array(&$img_dst), array_slice($wm,1));
+      $ua[0]=&$img_dst;
+      call_user_func_array('imagefilter', $ua);
+    } else {
     list($w_wm, $h_wm, $wm_type, $wm_img)=load_image(Conf::$orig.$wm[0]);
     $px=sizeof($wm>=4)?$wm[3]:0;
     $py=sizeof($wm>=5)?$wm[4]:0;
     imagecopy($img_dst, $wm_img, $px+($w0-$w_wm-2*$px)*$wm[1], $py+($h0-$h_wm+-2*$py)*$wm[2], 0, 0, $w_wm, $h_wm);
+    }
     } else { // tile
     list($w_wm, $h_wm, $wm_type, $wm_img)=load_image(Conf::$orig.$wm);
     imagesettile($img_dst, $wm_img);
@@ -94,28 +100,33 @@ function lock_release($l, $lock_fn) {
   return 0;
 }
 
-$fn=substr(strstr($_SERVER['PHP_SELF'], 'index.php'),10);
+$fn=false;
+$p=strrpos($_SERVER['SCRIPT_NAME'], '/');
+if ($p!=false) {
+  $fn=substr($_SERVER['REQUEST_URI'], $p+1);
+}
+if (!$fn) error('missing file name');
 $locks_d=dirname(__FILE__).'/locks/';
 @mkdir(dirname($locks_d), 0777, true);
 if (!is_dir(dirname($locks_d))) die("could not create directory [$lock_d]\n");
 $lock_fn=$locks_d.md5($fn);
-if (!$fn) error('missing file name');;
 if (strstr($fn, '..')) error('double dots not allowed');
 $abs_dst=dirname(__FILE__).'/'.$fn;
 
 $a=array();
-if (!preg_match('/^([0-9]+)x([0-9]+)\/(.*(?:\.(png|jpe?g|gif)))\.(png|jpg)/',$fn,$a)) error('wrong file name syntax');
-$w=$a[1];
-$h=$a[2];
-$e1=$a[4];
-$e2=$a[5];
+if (!preg_match('/^(([0-9]+)x([0-9]+)(?:_\w+)?)\/(.*(?:\.(png|jpe?g|gif)))\.(png|jpg)/i',$fn,$a)) error('wrong file name syntax');
+$preset=$a[1];
+$w=$a[2];
+$h=$a[3];
+$e1=strtolower($a[5]);
+$e2=$a[6];
 if ($e2=='png' && $e1!='png' && $e1!='gif')  error('wrong file name syntax');
-$src=Conf::$orig.'/'.$a[3];
+$src=Conf::$orig.'/'.$a[4];
 if (!file_exists($src)) error('original file does not exists!');
 
 $l=lock_aquire($lock_fn);
 if (!file_exists($abs_dst)) {
-  save_image_file(($e2=='png')?'png':'jpeg', $src, $abs_dst."~", $w, $h);
+  save_image_file(($e2=='png')?'png':'jpeg', $src, $abs_dst."~", $preset, $w, $h);
   rename($abs_dst."~", $abs_dst);
 }
 if ($e2=='png') header('Content-Type: image/png', TRUE, 200);
